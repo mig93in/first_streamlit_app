e8aeef38ac44ac7648c8ef14c388e9e80b47d6f5
mig93in <mig93in@gmail.com>
2022-07-01 07:14:34 -0500

    Update streamlit_app.py
    Added header and Menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.title('My Parents new healthy diner')
 
+st.header('Breakfast Menu')
+
+st.text('Hard Boiled Eggs')
+st.text('Kale + Spinach Sandwich')
+st.text('Free Range Egg Omelette')
+
 
