fd15e46b8690d50b1334f7ad70193655c3a22965
mig93in <mig93in@gmail.com>
2022-07-01 07:17:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ st.title('My Parents new healthy diner')
 
 st.header('Breakfast Menu')
 
-st.text('Hard Boiled Eggs')
-st.text('Kale + Spinach Sandwich')
+st.text('🐔 Hard Boiled Eggs')
+st.text('🥑🍞 Kale + Spinach Sandwich')
 st.text('Free Range Egg Omelette')
 
 
