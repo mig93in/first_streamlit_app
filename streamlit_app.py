fe1287a212dbcd675ee3b5affb76505c7d0a9bd0
mig93in <mig93in@gmail.com>
2022-07-01 07:34:55 -0500

    Update streamlit_app.py
    Set index for Multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ st.text('Free Range Egg Omelette')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 st.multiselect("Pick som fruits:", list(my_fruit_list.index))
 
