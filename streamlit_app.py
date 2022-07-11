dfffa404f9bacfed0e85f3e6446b3282b12e732b
mig93in <mig93in@gmail.com>
2022-07-11 08:15:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header('Fruityvice Fruit Added!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #st.text(fruityvice_response.json())
 
 
