d5c7686b068cafd84f58465977043dc45275fad0
mig93in <mig93in@gmail.com>
2022-07-14 14:47:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
       
 #st.stop()
 
-st.header("The fruit load list contains:")
+st.header("View our Fruit List - Add your favorites")
 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
